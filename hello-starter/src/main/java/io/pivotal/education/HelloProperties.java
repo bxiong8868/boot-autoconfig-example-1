a7da16c91da91f440fea90797d0cbd8f658c67b9
Eitan Suez <eitan.suez@gmail.com>
2018-01-15 11:22:39 -0600

    added spring boot configuration processor, and javadoc comments on fields in helloproperties

--- hello-starter/src/main/java/io/pivotal/education/HelloProperties.java
+++ hello-starter/src/main/java/io/pivotal/education/HelloProperties.java
@@ -4,7 +4,14 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix="hello")
 public class HelloProperties {
+  /**
+   * The prefix for the greeting
+   */
   private String prefix = "Hello";
+
+  /**
+   * Placed at the end of the greeting.
+   */
   private String suffix = "!";
 
   public String getPrefix() {
