2329b37f1af17f9d67999021d119a11029f31c1c
Eitan Suez <eitan.suez@gmail.com>
2018-01-14 22:46:31 -0600

    app now going through starter

---
+++ hello-starter/src/main/java/io/pivotal/education/HelloAutoConfig.java
@@ -0,0 +1,14 @@
+package io.pivotal.education;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloAutoConfig {
+
+  @Bean
+  HelloService helloService() {
+    return new ConsoleHelloService("Bonjour", ".");
+  }
+
+}
